630ab05ad338395281a0df10a93bf97d6db107a6
AlexisT37 <alexistroitzky@gmail.com>
2022-08-18 14:46:36 +0200

    use template for title and content

--- templates/homepage.php
+++ templates/homepage.php
@@ -1,33 +1,33 @@
-<!DOCTYPE html>
-<html>
+<?php $title = "Le blog de l'AVBN"; ?>
+<!-- we define a title for the page -->
 
-<head>
-    <meta charset="utf-8" />
-    <title>Le blog de l'AVBN</title>
-    <link href="style.css" rel="stylesheet" />
-</head>
+<!-- start a session with ob_start -->
+<?php ob_start(); ?>
+<h1>Le super blog de l'AVBN !</h1>
+<p>Derniers billets du blog :</p>
 
-<body>
-    <h1>Le super blog de l'AVBN !</h1>
-    <p>Derniers billets du blog :</p>
+<?php
+foreach ($posts as $post) { /* <!-- loops through the posts to display them --> */
+?>
+    <!-- class of news for the posts -->
+    <div class="news">
+        <h3>
+            <?= htmlspecialchars($post['title']); ?>
+            <em>le <?= $post['french_creation_date']; ?></em>
+        </h3>
+        <p>
+            <?= nl2br(htmlspecialchars($post['content'])); ?>
+            <br />
+            <!-- comments with the id of the post -->
+            <em><a href="post.php?id=<?= urlencode($post['identifier']) ?>">Commentaires</a></em>
+        </p>
+    </div>
+<?php
+}
+?>
+<!-- cleans the session with the method -->
+<?php $content = ob_get_clean(); ?>
 
-    <?php
-    foreach ($posts as $post) {
-    ?>
-        <div class="news">
-            <h3>
-                <?= htmlspecialchars($post['title']); ?>
-                <em>le <?= $post['french_creation_date']; ?></em>
-            </h3>
-            <p>
-                <?= nl2br(htmlspecialchars($post['content'])); ?>
-                <br />
-                <em><a href="post.php?id=<?= urlencode($post['identifier']) ?>">Commentaires</a></em>
-            </p>
-        </div>
-    <?php
-    }
-    ?>
-</body>
-
-</html>
+<!-- we require the layout -->
+<!-- it uses the content and title variables -->
+<?php require('layout.php') ?>
